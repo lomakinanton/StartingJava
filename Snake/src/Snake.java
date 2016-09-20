142a77a7c713ad9445a7830d50f08677bd6ef793
Lomakin Anton <lomakin.antonio@yandex.ru>
2016-09-20 17:30:46 +0500

    Начинаем писать змейку

---
+++ Snake/src/Snake.java
@@ -0,0 +1,14 @@
+import java.awt.Canvas;
+
+public class Snake extends Canvas{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1503268438955605275L;
+
+	public static void main(String[] args) {
+		
+	}
+
+}
