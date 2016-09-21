77ecf2ca98e403b139e232f0b69bc977869c4cd5
Lomakin Anton <lomakin.antonio@yandex.ru>
2016-09-21 15:29:41 +0500

    нужно составить псевдокод

--- Snake/src/gameSnake.java
+++ Snake/src/gameSnake.java
@@ -1,9 +1,39 @@
+import java.awt.Color;
+import java.util.ArrayList;
 
-public class gameSnake {
+import javax.swing.JFrame;
+
+public class gameSnake extends JFrame {
+	public gameSnake() {
+		setTitle("Game Snake");
+		setBounds(150,150, 300, 300);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new gameSnake();
 	}
+
+}
+
+class Snake {
+	private int length;
+	ArrayList<Point> snakeDots = new ArrayList<Point>();
+	int direction = 1;
+
+	public Snake(int length) {
+		this.length = length;
+	}
+
+}
+
+class Point {
+	int x;
+	int y;
+	Color color = Color.BLUE;
 
 }
