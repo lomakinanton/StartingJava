bdcf9856d027c8e2e9c6d0b1f6632827796d725d
Anton Lomakin <lomakin.antonio@yandex.ru>
2016-09-22 01:34:58 +0500

    Java 2 Lesson 1

---
+++ Lesson2_1/Course.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+public class Course {
+    private int difficultyLevel;
+
+    public Course(int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    void doIt(Team t){
+        ArrayList<Member> winners = new ArrayList<>();
+
+        for (Member m:t.getStaff()){
+            if (m.getSkillLevel()>=difficultyLevel){
+                winners.add(m);
+            }
+        }
+        t.setWinners(winners);
+    }
+}
