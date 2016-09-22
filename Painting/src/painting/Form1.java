51ecd1559c7590057936947bd7e365658fe46b6b
Lomakin Anton <lomakin.antonio@yandex.ru>
2016-09-22 15:48:16 +0500

    Научился рисовать, слушать мышь

--- Painting/src/painting/Form1.java
+++ Painting/src/painting/Form1.java
@@ -2,6 +2,7 @@ package painting;
 
 import java.awt.BasicStroke;
 import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -13,61 +14,104 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.image.BufferedImage;
+import java.nio.DoubleBuffer;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 class Form1 extends JFrame {
 
-    public Form1() throws HeadlessException {
+    @Override
+    public void paintAll(Graphics g) {
 
+        super.paintAll(g); //To change body of generated methods, choose Tools | Templates.
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.red);
+        g2.setStroke(new BasicStroke(10));
+        g.drawLine(x, y, x, y);
+        System.out.println(x + " " + y);
+    }
+
+    int x;
+    int y;
+
+    public Form1() {
         setTitle("Painting window");
-        setBounds(0, 0, 500, 500);
+        setBounds(0, 0, 300, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-        JLabel label = new JLabel();
-        label.setBackground(Color.red);
-        label.setForeground(Color.WHITE);
-        label.setOpaque(true);
-        label.setText("0:0");
-        
-        
-        
-        Canvas c = new Canvas();
-        c.setBackground(Color.BLUE);
-        
+        setResizable(false);
+        setLayout(new BorderLayout());
+        add(new myCanvas(), BorderLayout.CENTER);
+        setVisible(true);
+    }
+}
+
+class myCanvas extends Canvas {
+
+    int x;
+    int y;
+
+    BufferedImage back;
+    Graphics2D back_g2d;
+
+    ArrayList<Dot> dots = new ArrayList<>();
+
+    public myCanvas() {
+
+        back = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
+        back_g2d = (Graphics2D) back.getGraphics();
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == 1) {
+                    dots.add(new Dot(e.getX(), e.getY()));
+                    paint(getGraphics());
+                }
+            }
+        });
+
         addMouseMotionListener(new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                
+
+                dots.add(new Dot(e.getX(), e.getY()));
+                back_g2d.setPaint(Color.red);
+                back_g2d.setStroke(new BasicStroke(10));
+                back_g2d.drawLine(e.getX(), e.getY(), e.getX(), e.getY());
+
+                paint(getGraphics());
             }
 
             @Override
             public void mouseMoved(MouseEvent e) {
-                label.setText(String.valueOf(e.getX())+":"+String.valueOf(e.getY()));
             }
         });
-        
-        setLayout(new BorderLayout());
-        add(label, BorderLayout.NORTH);
-        setVisible(true);
-        //c.repaint();
-        
-    }
-  }
 
-class Canvas extends java.awt.Canvas{
+    }
+
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
-        
-        Graphics2D g2 = (Graphics2D) g;
-       // g2.setStroke(new BasicStroke(10));
-        g2.drawLine(0, 0, 500, 500);
-        g2.setColor(Color.red);
-        
+        //super.paint(g);
+        /*for (Dot dot : dots) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(Color.red);
+            g2.setStroke(new BasicStroke(10));
+            g2.drawLine(dot.x, dot.y, dot.x, dot.y);
+        }*/
+
+        g.drawImage(back, 0, 0, this);
     }
-    
-    
-    
+
+    private static class Dot {
+        int x;
+        int y;
+
+        public Dot(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
