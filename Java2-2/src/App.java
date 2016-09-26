324e81924584a8e71e16c1c061847f37b7f26999
Anton Lomakin <lomakin.antonio@yandex.ru>
2016-09-26 17:18:16 +0500

    Тренировки с исключениями

--- Java2-2/src/App.java
+++ Java2-2/src/App.java
@@ -2,73 +2,19 @@
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class App {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-
         try {
-            test7();
-        } catch (Exception ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            test1();
+            //test2();
+        } catch (MyEx e) {
+            System.out.println("!!!!!!!");
         }
-    }
-
-    public static void test() {
-        int a = 1;
-        int b = 0;
-
-        try {
-            System.out.println(a / b);
-        } catch (ArithmeticException e) {
-            System.out.println("Arithm");
-        } catch (RuntimeException e) {
-            System.out.println("Runtime");
-        } catch (Exception e) {
-            System.out.println("Exception");
-        } catch (Throwable e) {
-            System.out.println("Throwable");
-        }
-    }
-
-    public static void test2(int b) {
-        int a = 1;
-
-        try {
-            int x = a / b;
-        } catch (Throwable e) {
-            System.out.println("Throwable");
-        } finally {
-            System.out.println("Finnaly");
-        }
-        System.out.println("Finish");
 
     }
 
-    public static void test5(String s) {
-        if (s == null || s.length() == 0) {
-            throw new RuntimeException("RuntimeException empty string");
-        }
-    }
-
-    public static void test6(String s) throws Exception {
-        if (s == null || s.length() == 0) {
-            throw new Exception("Exception empty string");
-        }
-    }
-
-    public static void test7() {
-        int a = 1;
-        int b = 0;
-
-        try {
-            System.out.println(a / b);
-        } catch (ArithmeticException | NullPointerException e) {
-            System.out.println("Arithm");
-        }
-
+    public static void test1() {
+        throw new MyEx();
     }
 }
