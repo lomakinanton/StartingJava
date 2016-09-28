af4521e817e3509cd47a974ecf9472ad0e4c2fb0
Anton Lomakin <lomakin.antonio@yandex.ru>
2016-09-28 22:11:29 +0500

    Java 2 lesson 3

---
+++ Java2-3/src/Java23.java
@@ -0,0 +1,20 @@
+ /*
+ * @author Anton
+ */
+
+import java.util.ArrayList;
+
+public class Java23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       test1();
+    }
+
+    private static void test1() {
+        ArrayList<Integer> arr = new ArrayList<>();
+    }
+    
+}
