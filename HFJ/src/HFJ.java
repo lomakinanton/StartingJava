236964a449e2d94cc2fbd7e462c0a70a60aa4c15
Anton Lomakin <lomakin.antonio@yandex.ru>
2016-10-06 01:25:17 +0500

    Learning Formatting

--- HFJ/src/HFJ.java
+++ HFJ/src/HFJ.java
@@ -1,10 +1,15 @@
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class HFJ {
     
     public static void main(String[] args) {
-        System.out.println(String.format("%ty, %<tB, %<tY", new Date()));
+        Calendar c = Calendar.getInstance();
+        c.set(2016, 0, 1);
+        
+        
+        System.out.println(String.format("%td, %<tB, %<tY", c));
         
     }
    
