f53d6fa2c4d38cc1f575a7363ede56b2e05a34da
Anton Lomakin <lomakin.antonio@yandex.ru>
2016-10-06 01:10:16 +0500

    Learning Formatting

--- HFJ/src/HFJ.java
+++ HFJ/src/HFJ.java
@@ -1,32 +1,11 @@
 
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequencer;
-
+import java.util.Date;
 
 public class HFJ {
-
-    public HFJ() {
-    }
     
-
-    
-
     public static void main(String[] args) {
-           HFJ t = new HFJ();
-           t.go();
+        System.out.println(String.format("%ty, %<tB, %<tY", new Date()));
+        
     }
-
-    private void go() {
-        try {
-            Sequencer seq = MidiSystem.getSequencer();
-        } catch (MidiUnavailableException ex) {
-            ex.printStackTrace();
-        }
-    }
-
    
 }
