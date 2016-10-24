494dfb4f8c5cf1c1c358460475534711c5497a31
Anton Lomakin <lomakin.antonio@yandex.ru>
2016-10-25 00:17:51 +0500

    Разобрался с событием ControllerEvent

--- HFJ_Main/src/MiniMusicPlayer.java
+++ HFJ_Main/src/MiniMusicPlayer.java
@@ -30,7 +30,9 @@ public class MiniMusicPlayer implements ControllerEventListener {
 
             for (int i = 5; i < 61; i += 4) {
                 track.add(makeEvent(144, 1, i, 100, i));
+                
                 track.add(makeEvent(176,1,127,1,i));
+                               
                 track.add(makeEvent(128, 1, i, 100, i + 2));
             }
 
@@ -59,7 +61,7 @@ public class MiniMusicPlayer implements ControllerEventListener {
 
     @Override
     public void controlChange(ShortMessage event) {
-        System.out.println("la!");
+        System.out.println(event);
     }
 
 }
